af35e0e23e4037ee2c98e7010180330a0ec16be4
marcossnikel <marcosnikeldev@gmail.com>
2023-12-07 02:27:22 -0300

    Refactor database.js to use async/await syntax and add error handling

--- infra/database.js
+++ infra/database.js
@@ -1,23 +1,25 @@
+import { Client } from 'pg'
 
-import { Client } from "pg"
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+  })
+  await client.connect()
 
-
-async function query(queryObject){
-const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASSWORD,
-})
-await client.connect()
-const result = await client.query(queryObject)
-client.end()
-
-return result
+  try {
+    const result = await client.query(queryObject)
+    return result
+  } catch (error) {
+    console.log(error)
+  } finally {
+    await client.end()
+  }
 }
 
-
 export default {
-    query: query
+  query: query,
 }
