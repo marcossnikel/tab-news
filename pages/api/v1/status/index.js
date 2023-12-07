667ae67bb8abdf02b2c98032a06890fb7c3f84cd
marcossnikel <marcosnikeldev@gmail.com>
2023-12-07 02:27:27 -0300

    Refactor status API endpoint to include database information

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from '/infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows);
-  response.status(200).json({ key: 'the best' })
+  const updatedAt = new Date().toISOString()
+
+  const databaseVersionResult = await database.query('SHOW server_version;')
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version
+
+  const databaseOpenConnectionsResult = await database.query(
+    'SELECT count(*) FROM pg_stat_activity;',
+  )
+  const databaseOpenConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count
+
+  const databaseAvailableConnectionsResult = await database.query(
+    'SHOW max_connections;',
+  )
+  const databaseAvailableConnectionsValue =
+    databaseAvailableConnectionsResult.rows[0].max_connections
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        available_connections: databaseAvailableConnectionsValue,
+        open_connections: databaseOpenConnectionsValue,
+      },
+    },
+  })
 }
 
 export default status
